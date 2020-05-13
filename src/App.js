d8329420bd65a412da48804e0b0844bbc160f430
ty18881 <ty18881@gmail.com>
2020-05-13 08:25:36 -0400

    App.js - remove reference to localhost

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import CreateApplication from './components/CreateApplication';
 import ShowAllBorrowers from './components/ShowAllBorrowers';
 import ShowAllApplications from './components/ShowAllApplications';
 
-const baseURL = process.env.REACT_APP_BASEURL || "http://localhost:3000"
+const baseURL = process.env.REACT_APP_BASEURL;
 
 class App extends Component {
 
